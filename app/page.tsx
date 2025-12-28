01b6a864c14d3b030e8ab18861be346a3dd380ea
Jason Xie <165498921+Jason2657@users.noreply.github.com>
2025-12-28 16:54:49 +0100

    added new line

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
       <div className="p-8">
         <h1 className="text-3xl font-bold">Hey, I'm Jason!</h1>
+        <h2 className="text-xl font-bold">I'm a software engineer</h2>
       </div>
   );
 }
